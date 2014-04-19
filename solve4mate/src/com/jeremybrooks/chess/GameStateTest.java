d2625ea3e20c202f5749a8bdf30cab5668636b93
jeremy <jeremyis@live.com>
2014-04-18 20:52:45 -0700

    G5 remove duplication

--- solve4mate/src/com/jeremybrooks/chess/GameStateTest.java
+++ solve4mate/src/com/jeremybrooks/chess/GameStateTest.java
@@ -3,64 +3,46 @@ package com.jeremybrooks.chess;
 import static com.jeremybrooks.chess.Bitmap.*;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 public class GameStateTest {
 
 	private static final int NONE = 0;
+	private GameState gameState;
 
+	@Before
+	public void setup()
+	{
+		gameState = new GameState();
+	}
+	
+	@Test
+	public void testConstructor()
+	{
+		String expectedState = "8/8/8/8/8/8/8/8 w KQkq - 0 0";
+		String actualState = gameState.get();
+		assertEquals(expectedState, actualState);
+	}
+	
 	@Test
 	public void testMakeAndUndoWhitePieceMoves() {
-		String position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-		GameState g = new GameState();
-		g.set(position);
-		
-		String initialState = g.get();
-		assertEquals(position, initialState);
-		//Make move
-		int e4 = encodeMove(E2, E4, PIECE[PAWN]);
-		g.makeMove(e4, Bitmap.WHITE);
-		String afterE5 = g.get();
-		assertEquals("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1", afterE5);
-		
-		//Undo move
-		g.undoMove(e4, Bitmap.WHITE);
-		String afterUndoingE5 = g.get();
-		assertEquals(initialState, afterUndoingE5);
+		int moveE4 = encodeMove(E2, E4, PIECE[PAWN]);
+		String beforeMove = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+		String afterMove = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1";
+		assertMoveIsMadeAndUndone(WHITE, moveE4, beforeMove, afterMove);
 	}
 
 	@Test
 	public void testMakeAndUndoBlackPieceCaptures() {
-		String position = "rnbqkb1r/pppppppp/5n2/8/4P3/5N2/PPPPPPPP/RNBQKB1R b KQkq - 1 2";
-		GameState g = new GameState();
-		g.set(position);
-		
-		String stateBeforeMove = g.get();
-		assertEquals(position, stateBeforeMove);
-		//Make move
-		int knightTakeE4 = encodeMove(F6, E4, PIECE[KNIGHT], PIECE[PAWN]);
-		g.makeMove(knightTakeE4, BLACK);
-		String stateAfterMakeMove = g.get();
-		assertEquals("rnbqkb1r/pppppppp/8/8/4n3/5N2/PPPPPPPP/RNBQKB1R w KQkq - 0 3", stateAfterMakeMove);
-		
-		//Undo move
-		g.undoMove(knightTakeE4, BLACK);
-		String stateAfterUndoMove = g.get();
-		assertEquals(stateBeforeMove, stateAfterUndoMove);
+		int moveKnightTakeE4 = encodeMove(F6, E4, PIECE[KNIGHT], PIECE[PAWN]);
+		String beforeMove = "rnbqkb1r/pppppppp/5n2/8/4P3/5N2/PPPPPPPP/RNBQKB1R b KQkq - 1 2";
+		String afterMove = "rnbqkb1r/pppppppp/8/8/4n3/5N2/PPPPPPPP/RNBQKB1R w KQkq - 0 3";
+		assertMoveIsMadeAndUndone(BLACK, moveKnightTakeE4, beforeMove, afterMove);
 	}
 
-	private int encodeMove(int from, int to, int piece) {
-		return MoveGenerator.EncodeMove(from, to, piece, NONE, NONE);
-	}
 
-	private int encodeMove(int from, int to, int piece, int capturedPiece) {
-		return MoveGenerator.EncodeMove(from, to, piece, capturedPiece, NONE);
-	}
-
-	private int encodeMove(int from, int to, int piece, int capturedPiece, int promotionPiece) {
-		return MoveGenerator.EncodeMove(from, to, piece, capturedPiece, promotionPiece);
-	}
 
 
 	@Test @Ignore
@@ -88,5 +70,33 @@ public class GameStateTest {
 		fail("Not yet implemented");
 	}
 
+	private int encodeMove(int from, int to, int piece) {
+		return MoveGenerator.EncodeMove(from, to, piece, NONE, NONE);
+	}
+
+	private int encodeMove(int from, int to, int piece, int capturedPiece) {
+		return MoveGenerator.EncodeMove(from, to, piece, capturedPiece, NONE);
+	}
+
+	private int encodeMove(int from, int to, int piece, int capturedPiece, int promotionPiece) {
+		return MoveGenerator.EncodeMove(from, to, piece, capturedPiece, promotionPiece);
+	}
+
+	private void assertMoveIsMadeAndUndone(int sideToMove, int moveE4, String beforeMove, String afterMove) {
+		String position = beforeMove;
+		gameState.set(position);
+		
+		String initialState = gameState.get();
+		assertEquals(position, initialState);
+		//Make move
+		gameState.makeMove(moveE4, sideToMove);
+		String afterE5 = gameState.get();
+		assertEquals(afterMove, afterE5);
+		
+		//Undo move
+		gameState.undoMove(moveE4, sideToMove);
+		String afterUndoingE5 = gameState.get();
+		assertEquals(initialState, afterUndoingE5);
+	}
 
 }
