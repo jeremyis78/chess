136b8049cd8f600af2e28eb69326ab1b7f7d2966
jeremy <jeremyis@live.com>
2014-04-24 04:01:25 -0700

    extract a FEN parser class

---
+++ solve4mate/src/com/jeremybrooks/chess/FenParser.java
@@ -0,0 +1,268 @@
+package com.jeremybrooks.chess;
+
+import static com.jeremybrooks.chess.Bitmap.*;
+import static com.jeremybrooks.chess.Util.StrToSq;
+
+public class FenParser {
+	private static final char WHITE_ON_MOVE = 'w';
+	private static final char BLACK_ON_MOVE = 'b';
+	private static final String UNSET_FIELD = "-";
+	private static final int NUMBER_OF_FIELDS = 6;
+	private String originalFen;
+	private char[] board;
+	private boolean whiteToMove;
+	private boolean whiteShortCastle;
+	private boolean whiteLongCastle;
+	private boolean blackShortCastle;
+	private boolean blackLongCastle;
+	private int enPassantSquare;
+	private int halfMoveNumber;
+	private int moveNumber;
+	
+
+	public FenParser(){
+		board = new char[64];
+	}
+		
+	public void parse(String fen)
+	{
+		originalFen = fen;
+		String[] fields = originalFen.split(" ");
+        if (fields.length < NUMBER_OF_FIELDS){
+        	throw new IllegalArgumentException("FEN string '"+originalFen+"' "
+        			+ "needs six space-delimited fields: "
+        			+ "board onMove castlingOptions enPassantSquare halfMoveNumber moveNumber");
+        }
+        parseBoard(fields[0]);
+		parseOnMove(fields[1]);
+		parseCastleOptions(fields[2]);
+		parseEnPassantSquare(fields[3]);
+		parseHalfMoveNumber(fields[4]);
+		parseMoveNumber(fields[5]);
+	}
+
+	private void parseBoard(String field) {
+		String[] ranks = field.split("/");
+		if (ranks.length != 8)
+		{
+			throw new IllegalArgumentException("board must contain eight ranks");
+		}
+		boolean isKingPlaced[] = new boolean[2];
+		
+		//Start with the first rank
+		int currentSquare = Bitmap.A1; //keep track of the square we're on
+		for(int m = ranks.length - 1; m >= 0; m--)
+		{
+			String rank = ranks[m];
+			validateFiles(rank, m-8);
+			for (int n = 0; n < rank.length(); n++)
+			{
+				char boardCharacter = rank.charAt(n);
+				if (Character.isDigit(boardCharacter))
+				{
+					currentSquare = fillWithEmptySquares(currentSquare, boardCharacter);
+				}
+				else
+				{
+		            placePiece(isKingPlaced, currentSquare, boardCharacter);
+		            currentSquare++;
+		            //files++;
+				}
+			}
+		}
+	}
+
+	private static void validateFiles(String rankFen, int rankNumber)
+	{
+		int len = rankFen.length();
+		if (len == 0 || len > 8)
+		{
+			throw new IllegalArgumentException("board must contain eight squares on a rank, found: "+ rankFen);
+		}
+		
+		int filesRead = 0;
+		for(int i=0; i < len; i++)
+		{
+			char c = rankFen.charAt(i);
+			if (Character.isDigit(c))
+			{
+				filesRead += c - '0'; //represents multiple files
+				continue;
+			}
+			filesRead++; //represents a single file
+		}
+		if(filesRead != 8)
+		{
+			throw new IllegalArgumentException("board pieces and empty squares on rank #"+
+					Math.abs(rankNumber)+" do not fit on eight files: "+rankFen);
+		}
+	}
+
+	private void placePiece(boolean[] isKingPlaced, int sq, char c) {
+		switch(c){
+		case 'P':
+		case 'N':
+		case 'B':
+		case 'R':
+		case 'Q':
+		    board[sq] = c;
+		    break;
+		case 'K':
+		    if(isKingPlaced[Bitmap.WHITE])
+		    {
+		        throw new IllegalArgumentException("board has too many white kings");
+		    }
+		    board[sq] = c;
+		    isKingPlaced[Bitmap.WHITE] = true;
+		    break;
+		case 'p':
+		case 'n':
+		case 'b':
+		case 'r':
+		case 'q':
+			board[sq] = c;
+			break;
+		case 'k':
+		    if(isKingPlaced[Bitmap.BLACK])
+		    {
+		        throw new IllegalArgumentException("board has too many black kings");
+		    }
+		    board[sq] = c;
+		    isKingPlaced[Bitmap.BLACK] = true;
+		    break;
+		default: //illegal character
+		    throw new IllegalArgumentException("board contains invalid "
+		    		+ "piece '" + c + "'; allowed piece characters are: KkQqRrBbNnPp"); 
+		}
+	}
+
+	private int fillWithEmptySquares(int currentSquare, char numberOfEmptySquares) {
+		int emptysq = currentSquare;
+		//add digit's value to sq
+		currentSquare += numberOfEmptySquares - '0'; 
+         
+		//initialize those squares to empty
+		while (emptysq < currentSquare)
+		    board[emptysq++] = BOARD_EMPTY_SQUARE;
+		return currentSquare;
+	}
+
+	private void parseOnMove(String field) {
+		char onMove = field.trim().charAt(0);
+	    if (WHITE_ON_MOVE == onMove) {
+	        whiteToMove = true;
+	    } else if (BLACK_ON_MOVE == onMove) {
+	        whiteToMove = false;
+	    } else {
+	    	throw new IllegalArgumentException("onMove '"+field+"' is invalid; use 'w' for white or 'b' for black");
+	    }
+	}
+
+	private void parseCastleOptions(String field) {
+		if(field.length() > 4 || field.trim().isEmpty())
+		{
+			throw new IllegalArgumentException("castling options '" + field + "' "
+					+ "must not be empty or exceed four characters; use only characters from KQkq");
+		}
+        for (char option: field.toCharArray())
+        {
+        	switch(option)
+        	{
+        	case '-':
+        		whiteShortCastle = false;
+        		whiteLongCastle = false;
+        		blackShortCastle = false;
+        		blackLongCastle = false;
+        		break;
+        	case 'K':
+        		whiteShortCastle = true;
+        		break;
+        	case 'Q':
+        		whiteLongCastle = true;
+        		break;
+        	case 'k':
+        		blackShortCastle = true;
+        		break;
+        	case 'q':
+        		blackLongCastle = true;
+        		break;
+        	default:
+    			throw new IllegalArgumentException("castling options '" + field + "' "
+    					+ "are invalid; use only characters from KQkq");
+        	}
+        }
+	}
+
+	private void parseEnPassantSquare(String field) {
+	    if (UNSET_FIELD.equals(field)){
+	        enPassantSquare = NOSQUARE;
+	        return;
+	    }
+	    int square = StrToSq(field);
+	    String onMove = whiteToMove?"w":"b";
+	    if(whiteToMove && Util.notOnSixthRank(square))
+	    {
+			throw new IllegalArgumentException("given '"+onMove+"' to move, the "
+	        		+ "en passant square '"+field+"' ought to be on the 6th rank");	    	
+	    }
+	    if(!whiteToMove && Util.notOnThirdRank(square))
+	    {
+	        throw new IllegalArgumentException("given '"+onMove+"' to move, the "
+	        		+ "en passant square '"+field+"' ought to be on the 3rd rank");
+	    }
+        enPassantSquare = square;
+	}
+
+	private void parseHalfMoveNumber(String field) {
+	    int n = Integer.parseInt(field);
+	    if (n < 0){
+	    	throw new IllegalArgumentException("halfMoveNumber '"+field+"' "
+	    			+ "must be zero or greater");
+	    }
+	    halfMoveNumber = n;		
+	}
+
+	private void parseMoveNumber(String field) {
+	    int n = Integer.parseInt(field);
+	    if (n <= 0){
+	    	throw new IllegalArgumentException("moveNumber '"+field+"' must be greater than zero");
+	    }  
+	    moveNumber = n;
+	}
+
+	public char getBoardCharacter(int onSquare) {
+		return board[onSquare];
+	}
+
+	public boolean isWhiteToMove() {
+		return whiteToMove;
+	}
+
+	public boolean hasWhiteShortCastleOption() {
+		return whiteShortCastle;
+	}
+
+	public boolean hasWhiteLongCastleOption() {
+		return whiteLongCastle;
+	}
+
+	public boolean hasBlackShortCastleOption() {
+		return blackShortCastle;
+	}
+
+	public boolean hasBlackLongCastleOption() {
+		return blackLongCastle;
+	}
+
+	public int getEnPassantSquare() {
+		return enPassantSquare;
+	}
+
+	public int getHalfMoveNumber() {
+		return halfMoveNumber;
+	}
+
+	public int getMoveNumber() {
+		return moveNumber;
+	}
+}
---
+++ solve4mate/src/com/jeremybrooks/chess/FenParserTest.java
@@ -0,0 +1,194 @@
+package com.jeremybrooks.chess;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FenParserTest {
+
+	private FenParser parser;
+	
+	@Before
+	public void setUp()
+	{
+		parser = new FenParser();
+	}
+	
+	@Test
+	public void testParseWhiteToMove() {
+		String fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - - 5 30";
+		parser.parse(fen);
+		assertEquals('r', parser.getBoardCharacter(Bitmap.A8));
+		assertEquals('n', parser.getBoardCharacter(Bitmap.B8));
+		assertEquals('b', parser.getBoardCharacter(Bitmap.C8));
+		assertEquals('q', parser.getBoardCharacter(Bitmap.D8));
+		assertEquals('k', parser.getBoardCharacter(Bitmap.E8));
+		assertEquals('b', parser.getBoardCharacter(Bitmap.F8));
+		assertEquals('n', parser.getBoardCharacter(Bitmap.G8));
+		assertEquals('r', parser.getBoardCharacter(Bitmap.H8));
+		for(int square = Bitmap.A7; square >= Bitmap.H7; square--)
+		{
+			assertEquals('p', parser.getBoardCharacter(square));
+		}
+		for(int square = Bitmap.A6; square >= Bitmap.H3; square--)
+		{
+			assertEquals(Bitmap.BOARD_EMPTY_SQUARE, parser.getBoardCharacter(square));
+		}
+		for(int square = Bitmap.A2; square >= Bitmap.H2; square--)
+		{
+			assertEquals('P', parser.getBoardCharacter(square));
+		}
+		assertEquals('R', parser.getBoardCharacter(Bitmap.A1));
+		assertEquals('N', parser.getBoardCharacter(Bitmap.B1));
+		assertEquals('B', parser.getBoardCharacter(Bitmap.C1));
+		assertEquals('Q', parser.getBoardCharacter(Bitmap.D1));
+		assertEquals('K', parser.getBoardCharacter(Bitmap.E1));
+		assertEquals('B', parser.getBoardCharacter(Bitmap.F1));
+		assertEquals('N', parser.getBoardCharacter(Bitmap.G1));
+		assertEquals('R', parser.getBoardCharacter(Bitmap.H1));
+
+		assertTrue(parser.isWhiteToMove());
+		assertFalse(parser.hasWhiteShortCastleOption());
+		assertFalse(parser.hasWhiteLongCastleOption());
+		assertFalse(parser.hasBlackShortCastleOption());
+		assertFalse(parser.hasBlackLongCastleOption());
+		assertEquals(Bitmap.NOSQUARE, parser.getEnPassantSquare());
+		assertEquals(5, parser.getHalfMoveNumber());
+		assertEquals(30, parser.getMoveNumber());
+	}
+
+	@Test
+	public void testParseWithBlackCastlingEnPassant() {
+		String fen = "8/4P3/8/8/8/8/4P3/k6K b KQkq d3 3 22";
+		parser.parse(fen);
+		assertEquals('P', parser.getBoardCharacter(Bitmap.E7));
+		assertEquals('P', parser.getBoardCharacter(Bitmap.E2));
+		assertEquals('k', parser.getBoardCharacter(Bitmap.A1));
+		assertEquals('K', parser.getBoardCharacter(Bitmap.H1));
+		assertFalse(parser.isWhiteToMove());
+		assertTrue(parser.hasWhiteShortCastleOption());
+		assertTrue(parser.hasWhiteLongCastleOption());
+		assertTrue(parser.hasBlackShortCastleOption());
+		assertTrue(parser.hasBlackLongCastleOption());
+		assertEquals(Bitmap.D3, parser.getEnPassantSquare());
+		assertEquals(3, parser.getHalfMoveNumber());
+		assertEquals(22, parser.getMoveNumber());
+	}
+
+	@Test
+	public void testSetNotEnoughFields()
+	{
+		String notEnoughFields = "k6K/8/8/8/8/8/8/8 w KQkq";
+		String expectedError = "FEN string 'k6K/8/8/8/8/8/8/8 w KQkq' needs six space-delimited fields: board onMove castlingOptions enPassantSquare halfMoveNumber moveNumber";
+		assertInvalid(notEnoughFields, expectedError);
+	}
+	
+	@Test
+	public void testSetTooManyWhiteKings()
+	{
+		String tooManyWhiteKingsOnDifferentRanks = "2K5/8/8/8/8/8/8/7K w - - 0 1";
+		assertInvalid(tooManyWhiteKingsOnDifferentRanks, "board has too many white kings");
+	}
+
+	@Test
+	public void testSetTooManyBlackKings()
+	{
+		String tooManyBlackKingsOnSameRank = "8/k6k/8/8/8/8/8/8 w - - 0 1";
+		assertInvalid(tooManyBlackKingsOnSameRank, "board has too many black kings");
+	}
+
+	@Test
+	public void testSetTooManyRanksOnBoard()
+	{
+		String tooManyRanks = "8/8/8/8/8/8/8/8/8 w - - 0 1";
+		assertInvalid(tooManyRanks, "board must contain eight ranks");
+	}
+
+	@Test
+	public void testSetTooManyFilesOnBoard()
+	{
+		String tooManyFiles = "k8/8/8/8/8/8/8/8 w - - 0 1";
+		assertInvalid(tooManyFiles, "board pieces and empty squares on rank #8 do not fit on eight files: k8");
+	}
+	
+	@Test
+	public void testSetUnknownPiece()
+	{
+		String invalidPiece = "k6K/8/8/8/8/8/8/7z w - - 0 1";
+		assertInvalid(invalidPiece, "board contains invalid piece 'z'; allowed piece characters are: KkQqRrBbNnPp");
+	}
+	
+	@Test
+	public void testInvalidOnMove()
+	{
+		String badFen = "k6K/8/8/8/8/8/8/8 xx - - 0 1";
+		String expectedError = "onMove 'xx' is invalid; use 'w' for white or 'b' for black";
+		assertInvalid(badFen, expectedError);
+	}
+
+	@Test
+	public void testInvalidCastlingCharacters()
+	{
+		String badFen = "k6K/8/8/8/8/8/8/8 b KQxx - 0 2";
+		String expectedError = "castling options 'KQxx' are invalid; use only characters from KQkq";
+		assertInvalid(badFen, expectedError);
+	}
+
+	@Test
+	public void testTooManyCastlingCharacters()
+	{
+		String badFen = "k6K/8/8/8/8/8/8/8 b KQkqK - 0 2";
+		String expectedError = "castling options 'KQkqK' must not be empty or exceed four characters; use only characters from KQkq";
+		assertInvalid(badFen, expectedError);
+	}
+
+	@Test
+	public void testBlankCastlingOptions()
+	{
+		String badFen = "k6K/8/8/8/8/8/8/8 b  - 0 2";
+		String expectedError = "castling options '' must not be empty or exceed four characters; use only characters from KQkq";
+		assertInvalid(badFen, expectedError);
+	}
+
+	@Test
+	public void testInvalidEnPassant()
+	{
+		String badFen = "k6K/8/8/8/8/8/8/8 w - e3 0 1";
+		String expectedError = "given 'w' to move, the en passant square 'e3' ought to be on the 6th rank";
+		assertInvalid(badFen, expectedError);
+		
+		badFen = "k6K/8/8/8/8/8/8/8 b - h6 0 1";
+		expectedError = "given 'b' to move, the en passant square 'h6' ought to be on the 3rd rank";
+		assertInvalid(badFen, expectedError);
+	}
+
+	@Test
+	public void testHalfMoveNumberIsNotNegative()
+	{
+		String badFen = "k6K/8/8/8/8/8/8/8 b KQkq - -1 2";
+		String expectedError = "halfMoveNumber '-1' must be zero or greater";
+		assertInvalid(badFen, expectedError);
+	}
+
+	@Test
+	public void testMoveNumberIsAboveZero()
+	{
+		String badFen = "k6K/8/8/8/8/8/8/8 b KQkq - 1 0";
+		String expectedError = "moveNumber '0' must be greater than zero";
+		assertInvalid(badFen, expectedError);
+	}
+
+	private void assertInvalid(String position, String expectedError) {
+		try {
+			parser.parse(position);
+			fail(position+" did not throw '"+expectedError+"'");
+		} catch (IllegalArgumentException e) {
+			assertEquals(expectedError, e.getMessage());
+		}	
+	}
+
+}
