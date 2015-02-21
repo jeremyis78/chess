14933d47f201da844fefab0c885b2f8440aaf49c
Jeremy Brooks <jeremythinks@gmail.com>
2015-02-20 20:40:25 -0800

    add setter for displayer

--- solve4mate/src/com/jeremybrooks/chess/util/LongDisplayer.java
+++ solve4mate/src/com/jeremybrooks/chess/util/LongDisplayer.java
@@ -5,10 +5,18 @@ public class LongDisplayer extends AbstractDisplayer {
 	private static final char SET_BIT_CHAR = 'X';
 	private long bitboard;
 	
+	public LongDisplayer() {
+		super();
+	}
+
 	public LongDisplayer(long bitboard){
 		this.bitboard = bitboard;
 	}
-	
+
+	public void setLong(long bitboard) {
+		this.bitboard = bitboard;
+	}
+
 	@Override
 	void appendPiece(int currentSquare) {
 		
